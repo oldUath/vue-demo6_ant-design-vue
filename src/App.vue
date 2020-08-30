ef92c8e25a368ed31e4172d2ec116c9894b466d4
oldDong <2232890213@qq.com>
2020-08-30 22:18:04 +0800

    ant-design-vue的学习

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-form
+      id="components-form-demo-normal-login"
+      :form="form"
+      class="login-form"
+      @submit.prevent="onSubmit"
+    >
+      <a-form-item>
+        <a-input
+          v-decorator="[
+          'userName',
+          { rules: [{ required: true, message: '你咋不写姓名呀!' }] },
+        ]"
+          placeholder="用户名"
+        >
+          <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)" />
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input
+          v-decorator="[
+          'password',
+          { rules: [
+            { required: true, message: '你咋不写密码呀!' },
+            { min:8, message: '密码最低8个字符!' }
+          ] },
+        ]"
+          type="password"
+          placeholder="密码"
+        >
+          <a-icon slot="prefix" type="lock" style="color:rgba(0,0,0,.25)" />
+        </a-input>
+      </a-form-item>
+      <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
+        <a-button type="primary" html-type="submit">Submit</a-button>
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  beforeCreate() {
+    this.form = this.$form.createForm(this, { name: "normal_login" });
+  },
+
+  methods: {
+    onSubmit(e) {
+      console.log(e);
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log("data", values);
+        }
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss">
